6789ffaadbb08304134cc81d5a6ee3c0491d2049
Madhurjya Dasgupta <dasguptamadhurjya@gmail.com>
2023-05-14 23:53:34 +0530

    Renamed typo

---
+++ charactolyzer/model.py
@@ -0,0 +1,70 @@
+from afinn import Afinn
+import nltk
+import string
+import re
+
+rude_threshold = -5
+satisfied_threshold = 2
+unsatisfied_threshold = -2
+angry_threshold = -5
+sad_threshold = -3
+happy_threshold = 4
+
+def predict(names, index = 0, score = 0):
+    if index == len(names):
+        return score/len(names)
+    text = names[index]
+    text = ''.join([char for char in text if char in string.printable])
+    text = text.lower()
+    words = nltk.word_tokenize(text)
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    filtered_words = [word for word in words if word not in stop_words]
+
+    afinn = Afinn()
+    score += afinn.score(text)
+    print(score)
+    return predict(names, index+1, score)
+
+def character(score):
+    if score <= rude_threshold:
+        emotion = 'rude'
+    elif score >= happy_threshold:
+        emotion = 'happy'
+    elif score >= satisfied_threshold:
+        emotion = 'satisfied'
+    elif score <= unsatisfied_threshold:
+        emotion = 'unsatisfied'
+    elif score <= angry_threshold:
+        emotion = 'angry'
+    elif score <= sad_threshold:
+        emotion = 'sad'
+
+    return emotion
+
+def main():
+    with open('uploads/test.txt', 'r') as file:
+        text = file.read()
+
+    pattern = re.compile(r'(\d{2}/\d{2}/\d{4}), (\d{2}:\d{2}) - (.+?): (.+)')
+
+    matches = pattern.findall(text)
+    analyze = dict()
+    for date, time, name, body in matches:
+        print(f'Date: {date}')
+        print(f'Time: {time}')
+        print(f'Name: {name}')
+        print(f'Body: {body}')
+        if name not in analyze:
+            analyze[name] = [body]
+        else:
+            analyze[name].append(body)
+
+    print(analyze)
+    scores = dict()
+    for i in analyze.keys():
+        print(i)
+        temp = analyze[i]
+        scores[i] = predict(temp)
+        scores[i] = character(scores[i])
+
+    return scores
